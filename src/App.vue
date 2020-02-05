bd26d4ec7532e2a5fc92d80e26e7d4c3cf0e7079
tanjinwei <374055608@qq.com>
2020-02-05 17:16:45 +0800

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <img src="./assets/top_head.png" style="width:100%">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/utils/http.js
@@ -0,0 +1,6 @@
+const axios = require('axios')
+
+const http = axios.create({
+    baseURL:'https://view.inews.qq.com/g2'
+})
+export default http;
